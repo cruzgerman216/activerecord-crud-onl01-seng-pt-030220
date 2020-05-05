10b745e08f87dfced93f5895d73f137a7de1e8fb
German Cruz <cruzgerman216@yahoo.com>
2020-05-05 17:30:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -78,8 +78,10 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.find_by(title: "Wat?")
+  movie.title = "Wat,huh?"
+  movie.update
+  movie
 end
 
 def can_update_multiple_items_at_once
