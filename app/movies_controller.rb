15239f52315edf0f71cc984fd901336865f07440
German Cruz <cruzgerman216@yahoo.com>
2020-05-05 17:33:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,7 +79,9 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   movie = Movie.find_by(title: "Wat?")
-  movie.update(title: "Way, huh?")
+  movie.update(title: "Wat, huh?")
+  movie = Movie.find_by(title: "Wat?")
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
