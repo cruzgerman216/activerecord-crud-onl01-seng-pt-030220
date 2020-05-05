8a6e6739010d6e34d88c42308d819731f47590ec
German Cruz <cruzgerman216@yahoo.com>
2020-05-05 17:29:27 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -71,7 +71,7 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title:"Awesome Flick")
   movie.title = "Even Awesomer Flick"
-  movie.update
+  movie.save
   movie
 end
 
